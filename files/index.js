bf32fb55dd1d414df8398a4b7aaa5da6d9ae45ed
eoowe <143455257+eoowe@users.noreply.github.com>
2024-03-18 16:08:52 +0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const { exec, execSync } = require('child_process');
-const port = process.env.SERVER_PORT || process.env.PORT || 3000;        
+const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;        
 const UUID = process.env.UUID || '986e0d08-b275-4dd3-9e75-f3094b36fa2a'; //若需要改UUID，需要在config.json里改为一致
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.zzx.free.hr';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOGI5NzI0MDgwZTU1ZTcwMzcwZmI3NDI4NzkyMmYzMWIiLCJ0IjoiOGNlY2VlYzQtYzZiNi00N2VkLThhZjItY2I4MThmMDkxZWJkIiwicyI6Ik5XWTFNV1ZsWm1NdFpEYzJZeTAwWkdSaExUbGtZall0TnpneVpqZ3haVE00WkRBNSJ9';
-const CFIP = process.env.CFIP || 'government.se';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.';     // 建议使用token，argo端口8080，cf后台设置需对应
+const ARGO_AUTH = process.env.ARGO_AUTH || '';
+const CFIP = process.env.CFIP || 'www.visa.com.tw';
 const NAME = process.env.NAME || 'Choreo';
 
 // root route
@@ -56,6 +56,9 @@ app.get('/sub', (req, res) => {
   }
 } else {
   console.log('NEZHA variable is empty, skip running');
+  setTimeout(() => {
+    runWeb();
+  }, 2000);
 }
 
 // run-xr-ay
@@ -92,4 +95,4 @@ function runServer() {
   });
 }
 
-app.listen(port, () => console.log(`App is listening on port ${port}!`));
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}!`));
